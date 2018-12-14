bd4d25679cb2d4af060fc1e0f715d0b7032c742c
Wes Hampson <WesleyHampson@gmail.com>
2018-12-14 13:00:28 -0600

    Add memory

---
+++ include/emu/mem.h
@@ -0,0 +1,96 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+ * lc3tools - An implementation of the LC-3 ISA and assorted tools.           *
+ * Copyright (C) 2018 Wes Hampson.                                            *
+ *                                                                            *
+ * This program is free software; you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation; either version 2 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details                                *
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+/*==============================================================================
+ *   File: include/emu/mem.h
+ * Author: Wes Hampson
+ *   Desc: Memory for the LC-3b.
+ *============================================================================*/
+
+#ifndef __MEM_H
+#define __MEM_H
+
+#include <lc3.h>
+
+/*
+ * Memory size in bytes.
+ */
+#define MEM_SIZE        (1 << 16)
+
+/*
+ * Number of bits per cell (word size).
+ */
+#define MEM_WIDTH       16
+
+/*
+ * Number of cells (word count).
+ */
+#define MEM_DEPTH       ((MEM_SIZE) / (MEM_WIDTH / 8))
+
+/*
+ * Number of clock cycles to wait before reading or writing to memory
+ * (to simulate slow hardware).
+ */
+#define MEM_DELAY       50
+
+/*
+ * Memory state.
+ */
+struct lc3mem {
+    unsigned int c;             /* busy counter */
+    int r_en;                   /* read enable flag */
+    int w_en;                   /* write enable flag */
+    lc3word d[MEM_DEPTH];       /* data (16-bit word addressable) */
+};
+
+/*
+ * Reset control signals.
+ */
+void mem_reset(void);
+
+/*
+ * Execute one clock cycle on the memory unit.
+ */
+void mem_tick(void);
+
+/*
+ * Get a value indicating whether memory is idle.
+ *
+ * @return      if memory is idle
+ *              0 if memory is busy performing a read or write
+ */
+int mem_ready(void);
+
+/*
+ * Read a word from memory.
+ *
+ * @param data  a pointer to store the value read
+ * @param addr  the address to read from
+ * @return      1 when reading is complete
+ *              0 while data is being read
+ */
+int mem_read(lc3word *data, lc3word addr);
+
+/* Write a word to memory.
+ *
+ * @param addr  the address to write to
+ * @param data  the data to write
+ * @param wmask a bitmask indicating which bits to overwrite
+ * @return      1 when writing is complete
+ *              0 while data is being written
+ */
+int mem_write(lc3word addr, lc3word data, lc3word wmask);
+
+#endif /* __MEM_H */
---
+++ src/emu/mem.c
@@ -0,0 +1,74 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+ * lc3tools - An implementation of the LC-3 ISA and assorted tools.           *
+ * Copyright (C) 2018 Wes Hampson.                                            *
+ *                                                                            *
+ * This program is free software; you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation; either version 2 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details                                *
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+/*==============================================================================
+ *   File: src/emu/main.c
+ * Author: Wes Hampson
+ *   Desc: Main memory for the LC-3b.
+ *         Memory is word-addressable, but individual bytes can be written by
+ *         manipulating the write mask.
+ *============================================================================*/
+
+#include <mem.h>
+
+static struct lc3mem m;
+
+void mem_reset(void)
+{
+    m.c = 0;
+    m.r_en = 0;
+    m.w_en = 0;
+}
+
+void mem_tick(void)
+{
+    if (m.c > 0) {
+        m.c--;
+    }
+}
+
+int mem_ready(void)
+{
+     return m.c == 0;
+}
+
+
+int mem_read(lc3word *data, lc3word addr)
+{
+    if (!m.r_en) {
+        m.r_en = 1;
+        m.c = MEM_DELAY;
+    }
+    else if (m.c == 0) {
+        m.r_en = 0;
+        *data = m.d[addr >> 1];
+    }
+
+    return !m.r_en;
+}
+
+int mem_write(lc3word addr, lc3word data, lc3word wmask)
+{
+    if (!m.w_en) {
+        m.w_en = 1;
+        m.c = MEM_DELAY;
+    }
+    else if (m.c == 0) {
+        m.w_en = 0;
+        m.d[addr >> 1] = (m.d[addr >> 1] & ~wmask) | (data & wmask);
+    }
+
+    return !m.w_en;
+}
