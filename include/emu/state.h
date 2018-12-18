edf3717cc3946ba2dd29ba0a67393baa2ae6016d
Wes Hampson <WesleyHampson@gmail.com>
2018-12-18 00:09:26 -0600

    Add license header

--- include/emu/state.h
+++ include/emu/state.h
@@ -1,3 +1,24 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+ * lc3tools - An implementation of the LC-3 ISA and assorted tools.           *
+ * Copyright (C) 2018 Wes Hampson.                                            *
+ *                                                                            *
+ * This program is free software; you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation; either version 2 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details                                *
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+/*==============================================================================
+ *   File: include/emu/state.h
+ * Author: Wes Hampson
+ *   Desc: LC-3 CPU state function headers.
+ *============================================================================*/
+
 #ifndef __STATE_H
 #define __STATE_H
 
