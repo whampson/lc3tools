a741b2c44397549e499cc750b1196c5c4a2689b2
Wes Hampson <WesleyHampson@gmail.com>
2019-06-13 21:39:31 -0700

    Second attempt at writing a parser... it still doesn't work
    But it's not done yet ;)

--- src/as/main.c
+++ src/as/main.c
@@ -31,67 +31,48 @@ int read_line(struct source_file *src)
     return src->line_len;
 }
 
-int read_token(struct source_file *src, char **tok, char delim)
+int get_opcode(char *mnemonic, int *opcode, int *operand_count)
 {
-    size_t i;
-    // char *tok;
-    char *tok_head;
-    char *tok_tail;
-    char c;
-    int char_seen;
-
-    if (src->line_pos >= src->line_len)
+    if (strcasecmp(mnemonic, "BR") == 0)
     {
-        return 0;
+        // TODO: nzp
+        *opcode = 0x0;
+        *operand_count = 1;
+        return 1;
+    }
+    else if (strcasecmp(mnemonic, "ADD") == 0)
+    {
+        *opcode = 0x1;
+        *operand_count = 3;
+        return 1;
+    }
+    else if (strcasecmp(mnemonic, "RTI") == 0)
+    {
+        *opcode = 0x8;
+        *operand_count = 0;
+        return 1;
     }
 
-    tok_head = &src->line[src->line_pos];
-    tok_tail = tok_head;
-    i = 0;
-    char_seen = 0;
-
-    while ((c = tok_head[i]) != '\0')
-    {
-        src->line_pos++;
-        if (isspace(c))
-        {
-            if (!char_seen)
-            {
-                tok_head++;
-                tok_tail++;
-                continue;
-            }
-            else if (c == '\n')
-            {
-                *tok_tail = '\0';
-                *tok = tok_head;
-                return 1;
-            }
-            else if (!isspace(delim))
-            {
-                i++;
-                continue;
-            }
-        }
-
-        char_seen = 1;
-        if (c == delim)
-        {
-            *tok_tail = '\0';
-            *tok = tok_head;
-            return 1;
-        }
-        i++;
-        tok_tail++;
-    }
-
+    *operand_count = 0;
     return 0;
 }
 
 int main(int argc, char *argv[])
 {
     struct source_file src;
-    char *tok;
+    char *tok_head;
+    char *tok_tail;
+    char *mnemonic;
+    char *label;
+    char *operands[3];
+    int opcode;
+    int operand_count;
+    int operand_idx;
+    int label_seen;
+    int mnemonic_seen;
+    int non_whitespace_seen;
+    int comment_seen;
+    int valid_mnemonic;
 
     memset(&src, 0, sizeof(struct source_file));
     src.file = fopen(TEST_DIR"/astest2.asm", "rb");
@@ -102,17 +83,113 @@ int main(int argc, char *argv[])
 
     while (read_line(&src) != -1)
     {
-        read_token(&src, &tok, ' ');
-        // read_token(&src, &tok, '\t');
+        printf(">> line %zu...\n", src.line_num);
 
-        printf("tok: %s\n", tok);
-        if (strcasecmp(tok, "ADD") == 0)
+        operand_count = 0;
+        operand_idx = 0;
+        label_seen = 0;
+        mnemonic_seen = 0;
+        non_whitespace_seen = 0;
+        comment_seen = 0;
+        valid_mnemonic = 0;
+
+        tok_head = src.line;
+        tok_tail = tok_head;
+
+        while (src.line_pos <= src.line_len)
         {
-            printf("ADD\n");
-            while ((read_token(&src, &tok, ',')) != 0)
+            switch (*tok_tail)
             {
-                printf("'%s'\n", tok);
+                case '\r':
+                {
+                    goto next_char;
+                }
+
+                case ',':
+                {
+                    *tok_tail = '\0';
+                    operands[operand_idx++] = tok_head;
+                    tok_head = tok_tail + 1;
+                    goto next_char;
+                }
+
+                case '\0':
+                case '\n':
+                case '\t':
+                case ' ':
+                {
+                    if (comment_seen)
+                    {
+                        goto next_char;
+                    }
+                    if (!non_whitespace_seen)
+                    {
+                        tok_head++;
+                        goto next_char;
+                    }
+
+                    non_whitespace_seen = 0;
+                    *tok_tail = '\0';
+                    if (!mnemonic_seen)
+                    {
+                        // Mnemonic
+                        if (get_opcode(tok_head, &opcode, &operand_count))
+                        {
+                            mnemonic_seen = 1;
+                            mnemonic = tok_head;
+                        }
+                    }
+                    if (!comment_seen && mnemonic_seen && operand_idx == operand_count - 1)
+                    {
+                        // Operand
+                        operands[operand_idx++] = tok_head;
+                    }
+                    if (!mnemonic_seen && !label_seen)
+                    {
+                        // Label
+                        label_seen = 1;
+                        label = tok_head;
+                    }
+                    tok_head = tok_tail + 1;
+                    goto next_char;
+                }
+
+                case ';':
+                {
+                    comment_seen = 1;
+                    goto next_char;
+                }
+
+                default:
+                {
+                    if (comment_seen)
+                    {
+                        goto next_char;
+                    }
+
+                    // TODO: restrict allowed characters
+                    non_whitespace_seen = 1;
+                    goto next_char;
+                }
             }
+
+        next_char:
+            tok_tail++;
+            src.line_pos++;
+        }
+
+    next_line:
+        if (label_seen)
+        {
+            printf("LABEL = '%s'\n", label);
+        }
+        if (mnemonic_seen)
+        {
+            printf("MNEMONIC = '%s'\n", mnemonic);
+        }
+        for (int i = 0; i < operand_count; i++)
+        {
+            printf("OPERAND%d = '%s'\n", i, operands[i]);
         }
     }
 
