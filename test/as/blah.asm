3212fd721983e27e9d755a3af54257d026a208fe
Wes Hampson <WesleyHampson@gmail.com>
2019-06-15 02:00:50 -0700

    More tweaks

--- test/as/blah.asm
+++ test/as/blah.asm
@@ -1,12 +1,11 @@
 ; this is a comment (consider // as well)
-      ; this is a comment with leading whitespace!
 jmp r0
 
 label1 add r0,r1,r3
 label2 xor r4,r5,#6
-label3 and r6,r7,x1C ; with a trailing comment!
+label3 and r6,r7,x1C ; this is a trailing comment!
 
-label4  ; now we've got a macro and newline!
+label4
     .fill xA5A5
     .blkw 50
     .stringz "Hello, world!"
@@ -17,8 +16,8 @@ label               ; address is 0x46 (I think... & if program above doesn't cha
 ADD R5, R5, R5      ; 0001 1011 0100 0101   = 0x1B45
 ADD R5, R5, #-1     ; 0001 1011 0111 1111   = 0x1B7F
 
-AND reg, reg, R5    ; 0101 1011 0100 0101   = 0x5B45
-AND reg, reg, x1F   ; 0101 1011 0111 1111   = 0x5B7F
+AND R5, R5, R5      ; 0101 1011 0100 0101   = 0x5B45
+AND R5, R5, x1F     ; 0101 1011 0111 1111   = 0x5B7F
 
 BR label            ; 0000 111 000000000    =
 BRn label
@@ -27,7 +26,7 @@ BRp label
 BRnz label
 BRnp label
 BRzp label
-BRnzp label     ; same as BR (unconditional branch)
+BRnzp label
 
 JMP R5
 
@@ -53,8 +52,14 @@ STB R5, R5, label
 STW R5, R5, label
 STI R5, R5, label
 
-; expand on these (HALT, PUTS, etc...)
-TRAP x25
+TRAP x08
 
 XOR R5, R5, R5
 XOR R5, R5, x1F
+
+GETC        ; TRAP x20
+OUT         ; TRAP x21
+PUTS        ; TRAP x22
+IN          ; TRAP x23
+PUTSP       ; TRAP x24
+HALT        ; TRAP x25
