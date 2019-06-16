449720d9e3683cbfeab15f37562fb09e60626d2d
Wes Hampson <WesleyHampson@gmail.com>
2019-06-15 22:33:05 -0700

    Tweak test dir again

--- test/as/syntax.asm
+++ test/as/syntax.asm
@@ -1,3 +1,8 @@
+;
+; Syntax test.
+; Will probably cause the CPU to run off into the weeds if executed :D
+;
+
 START:                      ; label by itself
 BEGIN:  RET                 ; label w/ opcode, no operands
 
@@ -14,8 +19,16 @@ BEGIN:  RET                 ; label w/ opcode, no operands
         STW R3, R5, 2       ; register & register & address (literal)
         ADD R0, R1, R2      ; register & register & register
         ADD R3, R4, -2      ; register & register & immediate (dec)
-        ADD R5, R6, x1E     ; register & register & immediate (hex)
-        ADD R7, R0, START   ; NOT ALLOWED! ADD does not take an address
+        ADD R5, R6, 1E      ; register & register & immediate (hex)
+
+        ; weird spacing
+        ADD R0   , R5  ,  0xA   ; extra whitespace before ,
+        ADD R7   , R0  ,R6      ; whitespace mixture
+        ADD R4   ,R2  , R1      ; whitespace mixture
+		ADD	R7	,	R6,		R5  ; tabs
 
 DATA0:  .FILL 0xBEEF        ; pseudo-op w/ hex operand
 DATA1:  .FILL 1337          ; pseudo-op w/ dec operand
+HELLO:  .ASCII "Hello, world!", 13, 0
+
+FOO:
